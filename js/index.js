54bf6c2dfebe213b88c84bacd369291055a8d6ce
mdsoponhossain <mdsoponhossain2388@gmail.com>
2023-08-21 11:07:09 +0600

    last commit

--- js/index.js
+++ js/index.js
@@ -13,21 +13,42 @@ document.getElementById("first-card").addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name1}`
     const totalPrice1 = getTotalPrice('total-price', price1);
-    if (totalPrice1 >= 200) {
-        openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice1);
-        totalPayment('total', totalPrice1, discountAmount);
+
+    if (totalPrice1 >= 0) {
+        /* removing the disable  */
+        openButton('purchases-btn');
+        noDiscount('total', totalPrice1);
+
     }
+
+    if (totalPrice1 >= 200) {
+        /* removing the disable  */
+        openButton('coupon-btn');
+        noDiscount('total', totalPrice1);
+
+    }
+
     else {
         noDiscount('total', totalPrice1);
+
     }
-
-
 })
 
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price1 = getThePrice('p-one');
+        const finalP1 = price1 - price1;
+        const totalPrice1 = getTotalPrice('total-price', finalP1);
+        const finalTotal = totalPrice1
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
 
-
-
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
 
 
 
@@ -44,16 +65,39 @@ document.getElementById('second-card').addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name2}`
     const totalPrice2 = getTotalPrice('total-price', price2);
+    if (totalPrice2 >= 0) {
+        /* removing the disable  */
+        openButton('purchases-btn');
+        noDiscount('total', totalPrice2);
+
+    }
+    /* removing the disable  */
     if (totalPrice2 >= 200) {
         openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice2);
-        totalPayment('total', totalPrice2, discountAmount);
     }
     else {
         noDiscount('total', totalPrice2);
     }
-
 })
+
+
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price2 = getThePrice('p-two');
+        const finalP2 = price2 - price2;
+        const totalPrice2 = getTotalPrice('total-price', finalP2);
+        const finalTotal = totalPrice2
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
+
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
+
+
 
 
 
@@ -74,15 +118,41 @@ document.getElementById("third-card").addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name3}`
     const totalPrice3 = getTotalPrice('total-price', price3);
-    if (totalPrice3 >= 200) {
-        openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice3);
-        totalPayment('total', totalPrice3, discountAmount);
+
+    if (totalPrice3 >= 0) {
+        /* removing the disable  */
+        openButton('purchases-btn');
+        noDiscount('total', totalPrice3);
     }
+
+    if (totalPrice3 >= 200) {
+        /* removing the disable  */
+        openButton('coupon-btn');
+        noDiscount('total', totalPrice3);
+    }
+
     else {
         noDiscount('total', totalPrice3);
     }
 })
+
+
+
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price3 = getThePrice('p-three');
+        const finalP3 = price3 - price3;
+        const totalPrice3 = getTotalPrice('total-price', finalP3);
+        const finalTotal = totalPrice3
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
+
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
 
 
 
@@ -100,17 +170,48 @@ document.getElementById("forth-card").addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name4}`
     const totalPrice4 = getTotalPrice('total-price', price4);
-    if (totalPrice4 >= 200) {
-        openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice4);
-        totalPayment('total', totalPrice4, discountAmount);
-    }
-    else {
+    if (totalPrice4 >= 0) {
+        /* removing the disable  */
+        openButton('purchases-btn');
         noDiscount('total', totalPrice4);
     }
 
 
+    /* removing the disable  */
+    if (totalPrice4 >= 200) {
+        openButton('coupon-btn');
+        noDiscount('total', totalPrice4);
+
+    }
+    else {
+        noDiscount('total', totalPrice4);
+    }
 })
+
+
+
+
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price4 = getThePrice('p-four');
+        const finalP4 = price4 - price4;
+        const totalPrice4 = getTotalPrice('total-price', finalP4);
+        const finalTotal = totalPrice4
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
+
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
+
+
+
+
+
+
 
 
 /* fifth card */
@@ -125,15 +226,42 @@ document.getElementById("fifth-card").addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name5}`
     const totalPrice5 = getTotalPrice('total-price', price5);
+    if (totalPrice5 >= 0) {
+        /* removing the disable  */
+        openButton('purchases-btn');
+        noDiscount('total', totalPrice5);
+    }
+
+
     if (totalPrice5 >= 200) {
+        /* removing the disable  */
         openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice5);
-        totalPayment('total', totalPrice5, discountAmount);
+        noDiscount('total', totalPrice5);
     }
     else {
         noDiscount('total', totalPrice5);
     }
 })
+
+
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price5 = getThePrice('p-five');
+        const finalP5 = price5 - price5;
+        const totalPrice5 = getTotalPrice('total-price', finalP5);
+        const finalTotal = totalPrice5
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
+
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
+
+
+
 
 
 
@@ -150,13 +278,44 @@ document.getElementById("sixth-card").addEventListener('click', function () {
     nameSet.appendChild(p);
     p.innerHTML = `${count + 1}. ${name6}`
     const totalPrice6 = getTotalPrice('total-price', price6);
-    if (totalPrice6 >= 200) {
-        openButton('coupon-btn');
-        const discountAmount = findDiscount('discount', totalPrice6);
-        totalPayment('total', totalPrice6, discountAmount);
-    }
-    else {
+    /* removing the disable  */
+    if (totalPrice6 >= 0) {
+        openButton('purchases-btn');
         noDiscount('total', totalPrice6);
     }
 
+    if (totalPrice6 >= 200) {
+        /* removing the disable  */
+        openButton('coupon-btn');
+        noDiscount('total', totalPrice6);
+    }
+
+    else {
+        noDiscount('total', totalPrice6);
+    }
+})
+
+
+
+
+function theName() {
+    const inputField = document.getElementById('coupon-input');
+    const inputValue = inputField.value;
+    inputField.value = '';
+    if (inputValue === 'SELL200') {
+        const price6 = getThePrice('p-six');
+        const finalP6 = price6 - price6;
+        const totalPrice6 = getTotalPrice('total-price', finalP6);
+        const finalTotal = totalPrice6
+        console.log(finalTotal);
+        const discountAmount = findDiscount('discount', finalTotal);
+
+        totalPayment('total', finalTotal, discountAmount);
+    }
+}
+
+
+/* page reset function */
+document.getElementById('go-home-btn').addEventListener('click', function () {
+    location.reload();
 })
--- js/utilites.js
+++ js/utilites.js
@@ -1,21 +1,21 @@
 /* price collecting from field and find out the price numbers by function */
-function getThePrice(TextFieldId){
-    const priceField =document.getElementById(TextFieldId);
+function getThePrice(TextFieldId) {
+    const priceField = document.getElementById(TextFieldId);
     const priceString = priceField.innerText;
     const priceValue = parseFloat(priceString);
     return priceValue;
 }
 
 /* product name collecting from field and find out the name by function */
-function getTheProductName(nameFieldId){
-    const nameField =document.getElementById(nameFieldId);
+function getTheProductName(nameFieldId) {
+    const nameField = document.getElementById(nameFieldId);
     const productName = nameField.innerText;
     return productName;
 }
 
 /* calculating the total price function */
 
-function getTotalPrice(idTotalPrice,newAmount){
+function getTotalPrice(idTotalPrice, newAmount) {
     const totalPriceField = document.getElementById(idTotalPrice);
     const totalPriceString = totalPriceField.innerText;
     const totalPrice = parseFloat(totalPriceString);
@@ -24,41 +24,35 @@ function getTotalPrice(idTotalPrice,newAmount){
     return totalAmount;
 }
 
+
+
 /* discount calculating */
 
-function findDiscount( replacePosition,totalPrice){
+function findDiscount(replacePosition, totalPrice) {
     const replaceField = document.getElementById(replacePosition);
-    const discount= totalPrice*20/100;
-    const replaceValue =replaceField.innerText;
+    const discount = totalPrice * 20 / 100;
+    const replaceValue = replaceField.innerText;
     replaceField.innerText = discount;
     return discount;
-}  
-    
+}
 
-function totalPayment(replacePosition,totalPrice,totalDiscount){
+
+function totalPayment(replacePosition, totalPrice, totalDiscount) {
     const replaceField = document.getElementById(replacePosition);
-    const totalPaymentAmount = totalPrice-totalDiscount;
+    const totalPaymentAmount = totalPrice - totalDiscount;
     replaceField.innerText = totalPaymentAmount;
 }
-    
-
 
 
 /* if total cart is less than  200 tk (no discount function)  */
 
-function noDiscount(replacePosition,totalAmount){
+function noDiscount(replacePosition, totalAmount) {
     const replaceField = document.getElementById(replacePosition);
     replaceField.innerText = totalAmount;
 }
 
 /* removing button disable attribute function */
-function openButton(couponBtn){
-    const attributeField =document.getElementById(couponBtn);
+function openButton(couponBtn) {
+    const attributeField = document.getElementById(couponBtn);
     attributeField.removeAttribute('disabled');
 }
-    
-
-    
-
-
-    
